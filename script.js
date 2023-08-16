a7a63314163d568c0b6dbf685f2e4c4ab8b8d508
santosArnol <santosarnol@gmail.com>
2023-08-16 20:47:27 -0300

    Select and store necessary DOM elements

---
+++ script.js
@@ -0,0 +1,8 @@
+const equalsButtton = document.querySelector('[data-equals]');
+const clearButtton = document.querySelector('[data-clear]');
+const deleteButton = document.querySelector('[data-delete]');
+const numbersList = document.querySelectorAll('[data-number]')
+const operationsList = document.querySelectorAll('[data-operation]')
+
+const previousNumber = document.querySelector('.previousNumber');
+const currentNumber = document.querySelector('.currentNumber');

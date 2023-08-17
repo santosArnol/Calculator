c181ba218dad13b8aa0c222aa074926ab92d3eba
santosArnol <santosarnol@gmail.com>
2023-08-16 22:18:00 -0300

    Add delete and updateDisplay functions
    Removes the last number in currentNumber and updates display
    with new values respectively

--- script.js
+++ script.js
@@ -10,10 +10,22 @@ class Calculator {
         currentNumberDisplay.innerText = "";
     }
 
-    appendNumber(newNumber){
-        if (currentNumberDisplay.innerText.length > 15) return;
-        currentNumberDisplay.innerText += newNumber.target.innerText;
+    appendNumber(buttonText){
+        console.log(buttonText);
+        this.currentNumberDisplay.innerText += buttonText;
     }
+
+    delete(){
+        console.log(currentNumberDisplay.innerText.slice(0,-1));
+        this.currentNumberDisplay.innerText = currentNumberDisplay.innerText.slice(0,-1);
+    }
+
+    updateDisplay(){
+        previousNumberDisplay.innerText = this.previousNumberDisplay.innerText;
+        currentNumberDisplay.innerText = this.currentNumberDisplay.innerText;
+    }
+
+
 }
 
 const equalsButtton = document.querySelector('[data-equals]');
@@ -24,10 +36,23 @@ const operationsList = document.querySelectorAll('[data-operation]')
 const previousNumberDisplay = document.querySelector('.previousNumber');
 const currentNumberDisplay = document.querySelector('.currentNumber');
 
+
 const calculator = new Calculator(previousNumberDisplay,currentNumberDisplay);
 
-clearButtton.addEventListener('click',calculator.clear);
 
-numbersList.forEach(number => {
-    addEventListener('click',calculator.appendNumber)
+clearButtton.addEventListener('click', () => {
+    calculator.clear();
+    calculator.updateDisplay();        
+});
+
+deleteButton.addEventListener('click', () => {
+    calculator.delete();
+    calculator.updateDisplay();        
+});
+
+numbersList.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.appendNumber(button.innerText);
+        calculator.updateDisplay();        
+    })
 });

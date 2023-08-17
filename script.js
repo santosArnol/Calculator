cb9cc5185f9d6cac94a0edc0c61b11b5190c0b19
santosArnol <santosarnol@gmail.com>
2023-08-16 21:07:20 -0300

    Add Calculator class and first "clear" function

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
+class Calculator {
+    constructor (previousNumberDisplay,currentNumberDisplay){
+        this.previousNumberDisplay = previousNumberDisplay;
+        this.currentNumberDisplay = currentNumberDisplay;
+        this.clear()
+    }
+
+    clear(){
+        previousNumberDisplay.innerText = "";
+        currentNumberDisplay.innerText = "";
+    }
+}
+
 const equalsButtton = document.querySelector('[data-equals]');
 const clearButtton = document.querySelector('[data-clear]');
 const deleteButton = document.querySelector('[data-delete]');
 const numbersList = document.querySelectorAll('[data-number]')
 const operationsList = document.querySelectorAll('[data-operation]')
+const previousNumberDisplay = document.querySelector('.previousNumber');
+const currentNumberDisplay = document.querySelector('.currentNumber');
 
-const previousNumber = document.querySelector('.previousNumber');
-const currentNumber = document.querySelector('.currentNumber');
+const calculator = new Calculator(previousNumberDisplay,currentNumberDisplay);
+
+clearButtton.addEventListener('click',calculator.clear);
